7757d07fd2a5f29ae95c19c9d358ed3cff4fc697
Duck <teensupa@gmail.com>
2025-03-28 16:35:37 +0900

    Add files via upload

--- DATA/functions.py
+++ DATA/functions.py
@@ -100714,6 +100714,10 @@ def authenticate_google():
                 client_config = {
                     "installed": {
                         "client_id": client_id,
+                        "project_id":"root-unison-445102-g4",
+                        "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+                        "token_uri":"https://oauth2.googleapis.com/token",
+                        "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
                         "client_secret": client_secret,
                         "redirect_uris": ["http://localhost"]
                     }
