b88299c170926173f3b1e53459f82a374126a911
Duck <teensupa@gmail.com>
2025-04-01 11:38:17 +0900

    Add files via upload

--- DATA/functions.py
+++ DATA/functions.py
@@ -100718,9 +100718,11 @@ def authenticate_google():
             "web": {
                 "client_id": client_id,
                 "client_secret": client_secret,
-                "redirect_uris": [redirect_uri],
+                "redirect_uris": ["https://250327datachat-l88rlsemzbhauz4wveabv6.streamlit.app/"],
                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token"
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",+
+                "project_id":"root-unison-445102-g4"
             }
         }
         
